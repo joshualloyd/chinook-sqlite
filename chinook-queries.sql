5ea5f24acf640fb6eae6a4507a30add4cbc3b475
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-21 12:04:35 -0500

    wip finished query 12

--- chinook-queries.sql
+++ chinook-queries.sql
@@ -41,3 +41,32 @@ WHERE e.Title LIKE '%sales%'
 AND e.Title LIKE '%agent%';
 
 -- 8
+SELECT strftime('%Y', InvoiceDate) AS Year, sum(Total) AS Sales_Total
+FROM Invoice
+WHERE InvoiceDate LIKE '2009%'
+OR InvoiceDate LIKE '2011%'
+GROUP BY strftime('%Y', InvoiceDate);
+
+-- 9
+SELECT count(*) AS Invoice_Line_Count
+FROM InvoiceLine
+WHERE InvoiceId = 37;
+
+-- 10
+SELECT InvoiceId, count(*) AS Invoice_Line_Count
+FROM InvoiceLine
+GROUP BY InvoiceId;
+
+-- 11
+SELECT i.InvoiceId, t.Name
+FROM InvoiceLine i
+JOIN Track t ON i.TrackId = t.TrackId
+GROUP BY t.Name;
+
+-- 12
+SELECT i.InvoiceId AS Invoice_Number, t.Name AS Track_Name, ar.Name AS Artist_Name
+FROM InvoiceLine i
+JOIN Track t ON i.TrackId = t.TrackId
+JOIN Album al ON t.AlbumId = al.AlbumId
+JOIN Artist ar ON al.ArtistId = ar.ArtistId
+GROUP BY t.Name;
